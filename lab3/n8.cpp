c3ce3698c9d0cfb1733bc185d11ac236babbf7db
ufoproger <ufoproger@gmail.com>
2018-03-03 22:04:56 +0700

    Доработанный вариант 1-й программы 3-й лабораторной работы

--- lab3/n8.cpp
+++ lab3/n8.cpp
@@ -5,37 +5,50 @@
 
 using namespace std;
 
-HANDLE hCannibals = NULL;
-HANDLE hChef = NULL;
-vector < size_t > threadsData;
-size_t n, m;
+#define P(sem) WaitForSingleObject(sem, INFINITE)
+#define Vn(sem, n) ReleaseSemaphore(sem, n, NULL)
+#define V(sem) Vn(sem, 1)
 
-DWORD WINAPI ThreadFuncCannibal(PVOID pvParam = NULL) {
-	cout << "Выполняется поток каннибала." << endl;
+HANDLE hPot;
+HANDLE hChef;
+HANDLE hCout;
+
+DWORD WINAPI ThreadFuncCannibal(PVOID pvParam) {
+	size_t index = PtrToUlong(pvParam);
+
+	P(hCout);
+	cout << "Выполняется поток каннибала № " << index << "." << endl;
+	V(hCout);
 
 	for (;;) {
 		Sleep(rand() % 8 * 1000);
 
-		WaitForSingleObject(hCannibals, INFINITE);
-
-		cout << "Съел кусок мяса" << endl;
-
-		ReleaseSemaphore(hChef, 1, NULL);
+		P(hPot);
+		P(hCout);
+		cout << "Каннибал № " << index << " взял из котла кусок мяса..." << endl;
+		V(hCout);
+		V(hChef);
 	}
 
 	return 0;
 }
 
-DWORD WINAPI ThreadFuncChef(PVOID pvParam = NULL) {
+DWORD WINAPI ThreadFuncChef(PVOID pvParam) {
+	size_t m = PtrToUlong(pvParam);
+
+	P(hCout);
 	cout << "Выполняется поток повара." << endl;
+	V(hCout);
 
 	for (;;) {
 		for (size_t i = 0; i < m; ++i) {
-			WaitForSingleObject(hChef, INFINITE);
+			P(hChef);
 		}
 
-		cout << "Повар добавляет куски мяса в котёл." << endl;
-		ReleaseSemaphore(hCannibals, m, NULL);
+		P(hCout);
+		cout << "Добавленное поваром количество куском мяса в котёл: " << m << " шт." << endl;
+		V(hCout);
+		Vn(hPot, m);
 	}
 
 	return 0;
@@ -46,46 +59,31 @@ int main(int argc, char *argv[]) {
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
 
+	size_t n, m;
+
 	cout << "Количество дикарей и кусков мяса в котле: ";
 	cin >> n >> m;
 
-	vector < HANDLE > hThreads(n + 1, NULL);
+	if (n < 1 || m < 1) {
+		cout << "Значения не могут быть меньше единицы!" << endl;
+		return -1;
+	}
 
-	hCannibals = CreateSemaphore(
-		NULL,           // default security attributes
-		m,  // initial count
-		m,  // maximum count
-		NULL
-	);
+	hPot = CreateSemaphore(NULL, m, m, NULL);
+	hChef = CreateSemaphore(NULL, 0, m, NULL);
+	hCout = CreateSemaphore(NULL, 1, 1, NULL);
 
-	hChef = CreateSemaphore(
-		NULL,           // default security attributes
-		0,  // initial count
-		m,  // maximum count
-		NULL
-	);
+	vector < HANDLE > hThreads(n + 1);
 
 	for (size_t i = 0; i <= n; i++)
 	{
-		LPTHREAD_START_ROUTINE func = (i < n ? ThreadFuncCannibal : ThreadFuncChef);
+		LPTHREAD_START_ROUTINE func = (i < n) ? ThreadFuncCannibal : ThreadFuncChef;
+		size_t param = (i < n) ? (i + 1) : m;
 
-		hThreads[i] = CreateThread(
-			NULL,       // default security attributes
-			0,          // default stack size
-			func,
-			NULL,       // no thread function arguments
-			0,          // default creation flags
-			NULL); // receive thread identifier
-
-		if (hThreads[i] == NULL)
-		{
-			cout << "CreateThread error: " << GetLastError() << endl;
-			return 1;
-		}
+		hThreads[i] = CreateThread(NULL, 0, func, UlongToPtr(param), 0, NULL);
 	}
 
 	WaitForMultipleObjects(n + 1, hThreads.data(), TRUE, INFINITE);
-
 	system("pause");
 
 	return 0;
